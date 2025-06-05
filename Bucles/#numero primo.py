c2d4a47684b4962d8ec2dcb3def9d6f3742f341d
José Díaz <155374823+SolidusStrange@users.noreply.github.com>
2025-06-05 11:26:57 -0400

    Update #numero primo.py

--- Bucles/#numero primo.py
+++ Bucles/#numero primo.py
@@ -1,44 +1,44 @@
-#numero primo
+# Número primo
 contador = primos = no_primos = 0
 
+# Validar cantidad de números
 while True:
-  try:
-    cantidad = int(input("Ingrese la cantidad de números a verificar: "))
-  except ValueError:
-    print("Ingrese un valor númerico")
-    continue
-    
-  if (cantidad/round(cantidad))==1:
-    break
-  else:
-    print("Valor ingresado no es un entero")
+    try:
+        cantidad = int(input("Ingrese la cantidad de números a verificar: "))
+        break
+    except ValueError:
+        print("Debe ingresar un número entero.")
 
-while contador<cantidad:
-  try:
-    numero = int(input("Ingresa número: "))
-  except ValueError:
-    print("Ingrese un valor numérico")
-    
-  es_primo = True #bandera
+# Repetir por la cantidad ingresada
+while contador < cantidad:
+    # Validar número entero
+    try:
+        numero = int(input("Ingrese número: "))
+    except ValueError:
+        print("Debe ingresar un número entero.")
+        continue  # volver a pedir este número
 
-  #verificar si es numero primo
-  if numero >= 2:
-      for i in range(2, numero):          
-          if numero % i == 0:             
-              es_primo = False
-              break
-  else:   
-      es_primo = False
-      
-  #resultados
-  if es_primo:
-      print(numero, "es primo")
-      primos += 1
-  else:
-      print(numero, "no es primo.")
-      no_primos +=1
+    es_primo = True
 
-  contador += 1 #suma uno al contador
-    
-print(f"Se ingresaron {primos} números primos")
-print(f"Se ingresaron {no_primos} números primos")
+    # Verificar si es primo
+    if numero >= 2:
+        for i in range(2, numero):
+            if numero % i == 0:
+                es_primo = False
+                break
+    else:
+        es_primo = False
+
+    # Resultados
+    if es_primo:
+        print("Es primo.")
+        primos += 1
+    else:
+        print("No es primo.")
+        no_primos += 1
+
+    contador += 1
+
+# Mostrar resumen
+print(f"\nSe ingresaron {primos} números primos.")
+print(f"Se ingresaron {no_primos} números no primos.")
