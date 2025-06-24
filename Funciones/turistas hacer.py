13186c6a2ff8f1244c25166c64bd3cda86f99c4e
José Díaz <155374823+SolidusStrange@users.noreply.github.com>
2025-06-23 23:40:39 -0400

    Update turistas hacer.py
    La opción 1 debe estar implementada en una función llamada turistas_por_pais(pais) que recibe como parámetro el nombre del país y debe mostrar por pantalla una lista con los nombres de los turistas que pertenecen a ese país. 
    
    La opción 2 debe estar implementada en una función llamada turistas_por_mes(mes), la cual recibe un mes como parámetro y debe retornar el porcentaje de turistas que visito chile ese mes con respecto al total, redondeado a un decimal. El código principal debe ser el encargado de mostrar por pantalla el mensaje. 
    Es importante notar que el mes ingresado debe ser solo entre 1 y 12, y si no se cumple esto, el programa debe solicitar nuevamente el mes hasta que el usuario ingrese un valor dentro del rango solicitado. Ponga atención que, para un mes como febrero, solo se ingresa el valor 2 y no el 02. 
    
    La opción 3 debe estar implementada en una función llamada eliminar_turista(), la cual no recibe parámetros. La función debe permitir ingresar el nombre de un turista y eliminar ese turista siempre y cuando ese nombre exista. La eliminación debe funcionar independiente si el nombre que se ingresa está escrito en mayúsculas o minúsculas. Si se elimina un turista, la función debe mostrar el mensaje: "Turista eliminado con éxito.", y si no se elimina debe mostrar el mensaje: "Turista no encontrado. No se pudo eliminar."
    
    La opción 4 debe terminar el programa, mostrando el mensaje: "Programa terminado..."

--- Funciones/turistas hacer.py
+++ Funciones/turistas hacer.py
@@ -1,36 +1,58 @@
-turistas = {
-    "nombre": "John Doe", "pais": "Estados Unidos", "fecha": "12-01-2024",
-    "nombre": "Emily Smith", "pais": "Estados Unidos", "fecha": "23-03-2024",
-    "nombre": "Michael Brown", "pais": "Estados Unidos", "fecha": "05-07-2023",
-    "nombre": "Jessica Davis", "pais": "Estados Unidos", "fecha": "15-11-2024",
-    "nombre": "Carlos Garcia", "pais": "Mexico", "fecha": "10-05-2024",
-    "nombre": "Maria Lopez", "pais": "Mexico", "fecha": "08-12-2023",
-    "nombre": "Joao Silva", "pais": "Brasil", "fecha": "20-06-2024",
-    "nombre": "Ana Santos", "pais": "Brasil", "fecha": "03-10-2023",
-    "nombre": "Martin Fernandez", "pais": "Argentina", "fecha": "13-02-2023",
-    "nombre": "Sofia Gomez", "pais": "Argentina", "fecha": "07-04-2024",
-    "nombre": "Julian Martinez", "pais": "Argentina", "fecha": "19-09-2023",
-    "nombre": "Agustin Morales", "pais": "Argentina", "fecha": "28-03-2024"
-}
+turistas = [
+    {"nombre": "John Doe", "pais": "Estados Unidos", "fecha": "12-01-2024"},
+    {"nombre": "Emily Smith", "pais": "Estados Unidos", "fecha": "23-03-2024"},
+    {"nombre": "Michael Brown", "pais": "Estados Unidos", "fecha": "05-07-2023"},
+    {"nombre": "Jessica Davis", "pais": "Estados Unidos", "fecha": "15-11-2024"},
+    {"nombre": "Carlos Garcia", "pais": "Mexico", "fecha": "10-05-2024"},
+    {"nombre": "Maria Lopez", "pais": "Mexico", "fecha": "08-12-2023"},
+    {"nombre": "Joao Silva", "pais": "Brasil", "fecha": "20-06-2024"},
+    {"nombre": "Ana Santos", "pais": "Brasil", "fecha": "03-10-2023"},
+    {"nombre": "Martin Fernandez", "pais": "Argentina", "fecha": "13-02-2023"},
+    {"nombre": "Sofia Gomez", "pais": "Argentina", "fecha": "07-04-2024"},
+    {"nombre": "Julian Martinez", "pais": "Argentina", "fecha": "19-09-2023"},
+    {"nombre": "Agustin Morales", "pais": "Argentina", "fecha": "28-03-2024"}
+]
 
-def turistas_por_pais():
+def turistas_por_pais(pais):
     encontrado = False
-    pais = input("Ingrese el país a listar: ")
-    for paises in turistas:
-        if paises["pais"] == pais:
-            print(paises)
+    pais = pais.lower()
+    for turista in turistas:
+        if turista["pais"].lower() == pais:
+            print(f"Nombre: {turista['nombre']}, País: {turista['pais']}, Fecha de ingreso: {turista['fecha']}")
             encontrado = True
 
-    if encontrado == False:
-        print("País no encontrado")
+    if not encontrado:
+        print("No se encontraron turistas en ese país.")
 
-def turistas_por_mes():
-    pass
+
+def turistas_por_mes(mes):
+    total_turistas = len(turistas) #el largo de la lista para el %
+    contador_mes = 0
+
+    for turista in turistas:
+        mes_turista = int(turista["fecha"][3:5]) #extrae de la fecha exactamente el mes y lo transforma a entero quitando el 0
+        if mes_turista == mes:
+            contador_mes += 1
+
+    if contador_mes > 0:
+        porcentaje = round((contador_mes/total_turistas)*100,1) #round(variable,cantidad decimales)
+        return porcentaje
+    else:
+        return 0.0 #si no es mayor que cero, devolverá 0.0%
 
 def eliminar_turista():
-    pass
+    turista_eliminado = input("Ingrese el turista a eliminar de la lista.").lower()
+    encontrado = False
+    for turista in turistas:
+        if turista["nombre"].lower() == turista_eliminado:
+            turistas.remove(turista)
+            print(f"El turista {turista['nombre']} ha sido eliminado con éxito")
+            encontrado = True
+            break
 
-opcion = 0
+    if not encontrado:
+        print("Turista no encontrado. No se pudo eliminar")
+
 while True:
     print("MENU PRINCIPAL")
     print("1. Turistas por país")
@@ -38,21 +60,35 @@ while True:
     print("3. Eliminar turista")
     print("4. Salir")
 
-    try: 
+    try:
         opcion = int(input("Ingrese la opción que desea: "))
     except ValueError:
         print("Ingrese un valor numérico")
         continue
 
-    if opcion==1:
-        turistas_por_pais()
-            
-    elif opcion==2:
-        turistas_por_mes()
-    
-    elif opcion==3:
+    if opcion == 1:
+        pais = input("Ingrese el país a listar: ")
+        turistas_por_pais(pais)
+
+    elif opcion == 2:
+        while True:
+            try:
+                mes = int(input("Ingrese el número del mes que desea contar: "))
+                if mes>=1 and mes<=12:
+                    porcentaje = turistas_por_mes(mes)
+                    print(f"El {porcentaje}% de los turistas visitó Chile en el mes {mes}.\n")
+                    break
+                else:
+                    print("Ingrese número dentro del rango de meses(1 a 12).")
+            except ValueError:
+                print("Ingrese un valor numérico.")
+
+    elif opcion == 3:
         eliminar_turista()
-            
-    elif opcion==4:
-        print("Saliendo del programa")
+
+    elif opcion == 4:
+        print("Programa terminado...")
         break
+
+    else:
+        print("Ingrese una opción válida")
