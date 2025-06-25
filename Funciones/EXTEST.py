d59aacf92651a7f9d31e4b43e2d741b5cee9da92
José Díaz <155374823+SolidusStrange@users.noreply.github.com>
2025-06-25 16:28:40 -0400

    Update EXTEST.py
    Haga un programa que permita generar un menú de gestión de entradas para el Concierto de Trap con el “Conejo Simpático”. El menú principal debe permitir mostrar cuatro opciones: comprar entrada, consultar comprador, cancelar compra y salir del programa. Todas las opciones del menú deben estar implementadas mediante funciones separadas del código principal (main).
    
    Al ingresar a la opción de comprar entrada, se debe permitir ingresar el nombre del comprador, el tipo de entrada y el código de confirmación por separado. Para que la compra sea exitosa se deben cumplir las siguientes condiciones: el nombre del comprador no debe estar repetido; el tipo de entrada solo puede ser “G” (General) o “V” (VIP); y el código de confirmación debe tener un largo mínimo de seis caracteres, contener al menos una letra mayúscula, al menos un número y no puede tener espacios en blanco. En caso de cumplir todas estas condiciones, la entrada se registra exitosamente.
    
    Al seleccionar la opción de consultar comprador, el programa debe permitir buscar compradores mediante su nombre. Si el comprador existe, se deben mostrar los datos asociados, es decir, el tipo de entrada y el código de confirmación. Si no existe, debe mostrar un mensaje indicando que “El comprador no se encuentra.”
    
    La opción de cancelar compra debe permitir eliminar la compra y toda la información asociada mediante el ingreso del nombre del comprador por teclado. Si la compra se cancela correctamente, debe mostrarse el mensaje “¡Compra cancelada!”. Si el comprador no existe, debe mostrarse “No se pudo cancelar la compra.”
    
    Finalmente, al ingresar la opción de salir, el programa debe terminar mostrando el mensaje “Programa terminado...”. En caso de que se ingrese una opción distinta a las permitidas (1, 2, 3 o 4), debe mostrarse el mensaje “Debe ingresar una opción válida!!”.

--- Funciones/EXTEST.py
+++ Funciones/EXTEST.py
@@ -1,15 +1,106 @@
 # Funciones
+concierto = []
+compras_canceladas = []
+
 def comprar_entrada():
-    # Aquí irá la lógica para registrar un comprador
-    pass
+    #VERIFICAR QUE NO ESTÉ REPETIDO EN LA LISTA
+    while True:
+        repetido = False
+        nombre = input("Ingrese su nombre: ")
+        for publico in concierto:
+            if publico["nombre"] == nombre:
+                print("El comprador ya está registrado.")
+                repetido = True
+                break
+
+        if not repetido:
+            break        
+    #VERIFICAR QUE SEA G O V LO INGRESADO
+    while True:
+        entrada = input("Tipo de entrada (G para General / V para VIP): ").upper()
+        if entrada == "G" or entrada == "V":
+            break
+        else:
+            print("Tipo inválido. Ingrese V o G.")
+
+    #VERIFICAR QUE EL CODIGO DE CONFIRMACIÓN CUMPLA CON TODAS LAS CONDICIONES
+    while True:
+        codigo_confirmacion = input("Ingrese el código de confirmación")
+
+        #VERIFICAR CANTIDAD MINIMA DE CARACTERES
+        if len(codigo_confirmacion)<6:
+            print("El código debe tener al menos 6 caracteres.")
+            continue
+        #VERIFICAR QUE NO HAYA ESPACIOS EN BLANCO. SE PUEDE USAR REPLACE(" ","")
+        if " " in codigo_confirmacion:
+            print("El código no puede tener espacios en blanco.")
+            continue
+        #VERIFICAR QUE HAYA AL MENOS UNA MAYUSCULA RECORRIENDO LA PALABRA
+        mayuscula = False
+        for c in codigo_confirmacion:
+            if c.isupper(): #VERIFICA QUE HAYA MAYUSCULAS
+                mayuscula = True
+                break
+
+        if not mayuscula:
+            print("El código debe tener al menos una letra mayúscula.")
+            continue
+
+        #VERIFICAR QUE AL MENOS HAYA UN NUMERO
+        numero = False
+        for c in codigo_confirmacion:
+            if c.isdigit(): #VERIFICA QUE SEA UN NUM NO UN STR
+                numero = True
+                break
+
+        if not numero:
+            print("El código debe tener al menos un número")
+            continue
+
+        break #UNA VEZ SE CUMPLA TODO, PASA A AÑADIRSE AL DICCIONARIO Y A LA LISTA
+
+    comprador = {
+        "nombre": nombre,
+        "entrada": entrada,
+        "codigo": codigo_confirmacion
+                }
+
+    concierto.append(comprador)
+    print("Compra efectuada exitosamente!")
 
 def consultar_comprador():
-    # Aquí irá la lógica para consultar datos de un comprador
-    pass
+    nombre_comprador = input("Ingrese el nombre del comprador a consultar: ").lower()
+    encontrado = False
+
+    #RECORRER LA LISTA Y EVALUAR EN BASE A LA CLAVE "NOMBRE"
+    for compra in concierto:
+        if compra["nombre"].lower() == nombre_comprador:
+            print(f"Nombre: {compra['nombre']}")
+            print(f"Tipo de entrada: {compra['entrada']}")
+            print(f"Código de confirmación: {compra['codigo']}")
+            encontrado = True
+            break
+
+    if not encontrado:
+        print("El comprador no se encuentra.")
+
+
 
 def cancelar_compra():
-    # Aquí irá la lógica para cancelar una compra
-    pass
+    nombre_cancelar = input("Ingrese el nombre del comprador a cancelar: ").lower()
+    encontrado = False
+
+    #RECORRER LA LISTA Y EVALUAR NUEVAMENTE CON NOMBRE, PERO AQUÍ VA A AÑADIR A OTRA LISTA Y BORRAR DESPUES
+    for compra in concierto:
+        if compra["nombre"].lower() == nombre_cancelar:
+            compras_canceladas.append(compra)  
+            concierto.remove(compra)
+            print("¡Compra cancelada!")
+            encontrado = True
+            break
+
+    if not encontrado:
+        print("No se pudo cancelar la compra.")
 
 def mostrar_menu():
     print("MENU PRINCIPAL")
@@ -39,3 +130,4 @@ def main():
 # Ejecutar el programa
 if __name__ == "__main__":
     main()
+
