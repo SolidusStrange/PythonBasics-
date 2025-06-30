f0ccde170bac49cc88317cc9373f9d00ab192e49
José Díaz <155374823+SolidusStrange@users.noreply.github.com>
2025-06-30 16:48:44 -0400

    Update Ejercicio Practica funciones 1.py

--- Funciones/Ejercicio Practica funciones 1.py
+++ Funciones/Ejercicio Practica funciones 1.py
@@ -1,27 +1,113 @@
+from datetime import datetime
+
 libros = []
 prestados = []
 
 def agregar_libro():
-    nombre = input("Ingrese el nombre del libro que desea añadir: ")
-    autor = input("Ingrese el nombre del autor: ")
-    publicacion = int(input("Ingrese la fecha de publicacion: "))
-    genero = input("Ingrese el genero del libro a añadir: ")
+    nombre = input("Ingrese el nombre del libro que desea añadir: ").lower().strip()
 
+    while True:
+        autor = input("Ingrese el nombre del autor: ").lower().strip()
+        if all(c.isalpha() or c == " " for c in autor) and autor:
+            break
+        else:
+            print("Sólo puede usar letras y espacios. Ingrese nuevamente.")
+
+    while True:
+        publicacion = input("Ingrese la fecha en formato dd/mm/yyyy: ")
+        try:
+            fecha = datetime.strptime(publicacion, "%d/%m/%Y")
+            break
+        except ValueError:
+            print("Formato de fecha inválido. Use dd/mm/yyyy.")
+
+    genero = input("Ingrese el genero del libro a añadir: ").lower()
+
+    libro = {
+        "nombre": nombre,
+        "autor": autor,
+        "fecha_publicacion": fecha.date(),
+        "genero": genero,
+        "estado": "disponible"
+        }
+
+    libros.append(libro)
+    print("Libro agregado correctamente.")
 
 def listar_libros():
-    pass
+    print("----BIBLIOTECA----")
+    encontrados = [libro for libro in libros if libro["estado"] == "disponible"]
+
+    if not encontrados:
+        print("No hay libros disponibles en la biblioteca.")
+    else:
+        for i, libro in enumerate(encontrados, 1):
+            print(f"{i}. {libro['nombre'].capitalize()} - {libro['autor'].capitalize()} ({libro['fecha_publicacion']}) - {libro['genero'].capitalize()}")
+
 
 def prestar_libro():
-    pass
+    titulo = input("Ingrese el nombre título del libro que desea pedir prestado").lower()
+    encontrado = False
+    for libro in libros:
+        if titulo == libro["nombre"].lower():
+            encontrado = True
+            if libro["estado"] == "disponible":
+                print("Libro prestado con éxito.")
+                prestados.append(libro)
+                libro["estado"] = "prestado"
+                break
+            else:
+                print("El libro se encuentra prestado.")
+                break
+
+    if not encontrado:
+        print("El libro no se encuentra en la biblioteca. Revise si el título es el correcto")
 
 def devolver_libro():
-    pass
+    titulo = input("Ingrese el libro a devolver: ").lower()
+    encontrado = False
+    for libro in libros:
+        if libro["nombre"].lower() == titulo:
+            encontrado = True
+            libro["estado"] = "disponible"
+            prestados.remove(libro)
+            print("Libro devuelto exitosamente.")
+            break
+
+    if not encontrado:
+        print("El libro a devolver no se encuentra en la biblioteca. Revise el título ingresado")
 
 def buscar_libro():
-    pass
+    titulo = input("Ingrese el libro que busca: ").lower()
+    encontrado = False
+    for libro in libros:
+        if libro["nombre"].lower() == titulo:
+            encontrado = True
+            print(f"Nombre: {libro['nombre'].capitalize()}")
+            print(f"Autor: {libro['autor'].capitalize()}")
+            print(f"Fecha de Publicación: {libro['fecha_publicacion']}")
+            print(f"Genero: {libro['genero'].capitalize()}")
+            print(f"Estado: {libro['estado'].capitalize()}")
+            break
+
+    if not encontrado:
+        print("El libro consultado no se encuentra en la biblioteca.")
 
 def eliminar_libro():
-    pass
+    titulo = input("Ingrese el libro a eliminar: ").lower()
+    encontrado = False
+
+    for libro in libros:
+        if titulo == libro["nombre"].lower():
+            encontrado = True
+            libros.remove(libro)
+            if libro in prestados:
+                prestados.remove(libro)
+            print("Libro eliminado")
+            break
+
+    if not encontrado:
+        print("El libro a eliminar no se encuentra en la biblioteca")
 
 # Menú principal
 opcion = 0
